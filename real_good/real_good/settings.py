dc8e34f932e96d5c650ab20c3fad9d9fa81f4bfb
Arnaud Paran <paran.arnaud@gmail.com>
2019-11-03 13:27:04 +0100

    added postgresql management

--- real_good/real_good/settings.py
+++ real_good/real_good/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,6 +80,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] =\
+            dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
